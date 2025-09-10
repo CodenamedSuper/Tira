bbcebac21d48f8d3aa2042e9cdd6d16a0e2fd74b
Super1166 <110970817+CodenamedSuper@users.noreply.github.com>
2025-09-10 13:45:02 +0300

    Body and component classes

---
+++ Tira/src/level/Body.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tira;
+
+public class Body
+{
+    public string ID { get; set; } = string.Empty;
+    public List<Component> Components { get; set; } = new List<Component>();
+    public virtual void Start()
+    {
+
+    }
+    public void Add(Component component)
+    {
+        component.Parent = this;
+        component.Start();
+        Components.Add(component);
+    }
+    public T GetComponent<T>() where T : Component
+    {
+        return Components.FirstOrDefault(component => component is T) as T;
+    }
+    public bool HasComponent<T>() where T : Component
+    {
+        foreach (Component component in Components)
+        {
+            if (component is T) return true;
+        }
+
+        return false;
+    }
+    public Component Remove(Component component)
+    {
+        Components.Remove(component);
+
+        return component;
+    }
+    public virtual void Update()
+    {
+        Components.ForEach(component => component.Update());
+    }
+    public virtual void Draw()
+    {
+        Components.ForEach(component => component.Draw());
+    }
+}
---
+++ Tira/src/level/component/Component.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tira;
+
+public class Component
+{
+    public Body Parent { get; set; }
+
+    public virtual void Start()
+    {
+
+    }
+    public virtual void Update()
+    {
+
+    }
+    public virtual void Draw()
+    {
+
+    }
+}
